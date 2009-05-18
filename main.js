36ffbfd0edbf1a68e2696ed39d1be84918dccd1c
Robert Thurnher <robi42@calypso.robert42.com>
2009-05-18 20:14:25 +0200

    Upgrade to current MySQL Connector/J JDBC driver (version 5.1.7).

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env helma
 
-// load DB connector lib
-addToClasspath('lib/mysql-connector-java-5.1.6-bin.jar');
-
 // main script to start application
 
 if (__name__ == "__main__") {
+    // load JDBC driver
+    addToClasspath('lib/mysql-connector-java-5.1.7-bin.jar');
     require("helma/webapp").start();
 }
